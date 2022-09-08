3066bbb95da345c6e86ace895d018eccc01b5f68
Gatwan <abigailgathigia@gmail.com>
2022-09-08 11:05:47 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char ary[] = "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n";
+	char ary[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fprintf(stderr, "%s", ary);
 	return (1);
