ee9d11434b73b409af1f2c764548c0ba551b0cd0
Gatwan <abigailgathigia@gmail.com>
2022-09-08 11:02:16 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (success)
+ */
+int main(void)
+{
+	char ary[] = "and that piece of art is useful\" -Dora Kopar, 2015-10-19\n";
+
+	fprintf(stderr, "%s", ary);
+	return (1);
+}
