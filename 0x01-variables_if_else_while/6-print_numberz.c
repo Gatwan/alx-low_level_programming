e86f24cf52d0c4a25f16a8669d4768c154afbc6c
Gatwan <abigailgathigia@gmail.com>
2022-09-09 08:44:31 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Numbers 1 to 9
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
