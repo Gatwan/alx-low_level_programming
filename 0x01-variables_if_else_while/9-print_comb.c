98a6448fafc0cb37cd2c395e1a2fcb3ea7bdb6bd
Gatwan <abigailgathigia@gmail.com>
2022-09-09 09:09:22 -0700

    Print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Single digit numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
