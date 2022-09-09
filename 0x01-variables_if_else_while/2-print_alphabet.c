62bb664d7dd3f960a8080058c1ee7ad1a59e1840
Gatwan <abigailgathigia@gmail.com>
2022-09-09 07:56:28 -0700

    printalphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+}
+putchar('\n');
+return (0);
+}
