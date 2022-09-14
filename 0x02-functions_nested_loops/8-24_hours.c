f1e8833a853892b17f571351f615374893cc4b74
Gatwan <abigailgathigia@gmail.com>
2022-09-14 09:03:42 -0700

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * jack_bauer -  prints every minute of the day of Jack Bauer
+ *
+ */
+void jack_bauer(void)
+{
+	int num1, num2, num3, num4;
+
+	for (num1 = 48; num1 <= 50; num1++)
+	{
+		for (num2 = 48; num2 <= 57; num2++)
+		{
+			if (num1 == 50 && num2 == 52)
+			{
+				break;
+			}
+			for (num3 = 48; num3 < 54; num3++)
+			{
+				for (num4 = 48; num4 <= 57; num4++)
+				{
+					_putchar(num1);
+					_putchar(num2);
+					_putchar(':');
+					_putchar(num3);
+					_putchar(num4);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
