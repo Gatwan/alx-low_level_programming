cc71c979015c1879f4d3095cfad1ae6da0069e9c
Gatwan <abigailgathigia@gmail.com>
2022-09-15 13:51:26 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - FizzBuzz
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int FizzBuzz;
+
+	for (FizzBuzz = 1; FizzBuzz <= 100; FizzBuzz++)
+	{
+		if (FizzBuzz == 0)
+		{
+			printf("Buzz");
+		}
+		else if (FizzBuzz % 3 == 0 && FizzBuzz % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (FizzBuzz % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (FizzBuzz % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", FizzBuzz);
+		}
+	}
+	printf("\n");
+	return (0);
+}
