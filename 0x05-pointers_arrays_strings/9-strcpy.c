880d686aa60773280243a3071edd8019a117fd43
Gatwan <abigailgathigia@gmail.com>
2022-09-19 08:57:36 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy - copy string
+ * @dest: buffer
+ * @src: string
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src);
+{
+	int c;
+
+	for (c = 0; *(src + c) != '\0'; c++)
+	{
+		*(dest + c) = *(src + c);
+	}
+	*(dest + c) = *(src + c);
+	return (dest);
+}
