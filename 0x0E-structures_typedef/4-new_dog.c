2cf3c3aef539e7e5f9f4f91440991cb614544a1d
Gatwan <abigailgathigia@gmail.com>
2022-10-11 15:46:07 +0000

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,37 +12,41 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *nina;
 	int namelen, ownerlen, a;
 
-	nina = malloc(sizeof(*nina));
-	if (nina == NULL || !(name) || !(owner))
+	if (name == NULL || age < 0 || owner == NULL)
+	return (NULL);
+
+	nina = malloc(sizeof(dog_t));
+	if (nina == NULL)
 	{
 		free(nina);
 		return (NULL);
 	}
+	for (namelen = 0; name[namelen] != '\0'; namelen++)
 
-	for (namelen = 0; name[namelen]; namelen++)
+	for (ownerlen = 0; owner[ownerlen] != '\0'; ownerlen++)
 
-	for (ownerlen = 0; owner[ownerlen]; ownerlen++)
-
-	nina->name = malloc(namelen + 1);
-	nina->owner = malloc(ownerlen + 1);
-
-	if (!(nina->name) || (nina->owner))
+	nina->name = malloc(sizeof(char) * (namelen + 1));
+	if (nina->name == NULL)
 	{
 		free(nina->name);
-		free(nina->owner);
 		free(nina);
 		return (NULL);
 	}
-
 	for (a = 0; a < namelen; a++)
-		nina->name[a] = name[a];
+	nina->name[a] = name[a];
 	nina->name[a] = '\0';
-
 	nina->age = age;
+	nina->owner = malloc(sizeof(char) * (ownerlen + 1));
+	if (nina->owner == NULL)
+	{
+		free(nina->owner);
+		free(nina->name);
+		free(nina);
+		return (NULL);
+	}
+	for (a = 0; a < ownerlen; ownerlen++)
+	nina->owner[a] = owner[a];
 
-	for (a = 0; a < ownerlen; a++)
-		nina->owner[a] = owner[a];
 	nina->owner[a] = '\0';
-
 	return (nina);
 }
