6006dd4e1ee84d8be180f1590e20330f40e80997
Gatwan <abigailgathigia@gmail.com>
2022-10-11 13:54:59 +0000

    A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+*new_dog - function that creates a new dog
+*@name: dog's name
+*@age: dog's age
+*@owner: dog's owner
+*Return: struct dog else NULL
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *newname = name, *newowner = owner;
+	dog_t nina, *new;
+
+	if (name == NULL || owner == NULL || age < 0)
+	return (NULL);
+
+	nina.name = newname;
+	nina.age = age;
+	nina.owner = newowner;
+
+	new = &nina;
+	if (new == NULL)
+	return (NULL);
+
+	return (new);
+}
