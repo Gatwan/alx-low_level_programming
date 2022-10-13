1872e2850fcf92160b5ee2b2f53c865c2a158d05
Gatwan <abigailgathigia@gmail.com>
2022-10-13 16:20:38 +0000

    A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,15 +12,23 @@
 */
 int main(int argc, char *argv[])
 {
-	int ans, a, b;
+	int a, b;
+	int (*ans)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
-		&& *argv[2] != '/' && *argv[2] != '%')
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	ans = get_op_function(argv[2]);
+
+	if (ans == NULL)
 	{
 		printf("Error\n");
 		exit(99);
@@ -29,15 +37,6 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	if ((*argv[2] == '/' && b == 0) ||
-		(*argv[2] == '%' && b == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	ans = (int)(get_op_func(argv[2])(a, b));
-
-	printf("%d\n", ans);
+	printf("%d\n", ans(a, b));
 	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <stdio.h>
 /**
 *op_add - adds two integers
 *
@@ -61,5 +60,10 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
