f5c91d78fd74273f77aaeed494a95fc301c8a601
Gatwan <abigailgathigia@gmail.com>
2022-10-14 10:38:38 +0000

    Beauty is variable, ugliness is constant

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+*sum_them_all - function that returns the sum of all its parameters
+*@n: parameter
+*Return: sum of all its parameters
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, cnt;
+
+	cnt = 0;
+
+	va_start(ap, n);
+
+	if (n == 0)
+	return (0);
+
+	for (i = 0; i < n; i++)
+	{
+		cnt += va_arg(ap, int);
+	}
+	va_end(ap);
+	return (cnt);
+
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,5 @@
+VARIADIC FUNCTIONS
+
+This project showcases solutions to various tasks provided that help us understand what are variadic functions. 
+The major macros used in these projects are va_start, va_arg and va_end.
+We also interact with the const type qualifier and we learn why it is used and how to use it.
