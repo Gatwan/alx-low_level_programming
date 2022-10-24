d547d8ecb3ba0b2bcec58626917d97a430e6a3f6
Gatwan <abigailgathigia@gmail.com>
2022-10-24 19:43:34 +0000

    assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,12 @@
-global main
-	extern printf
+global	main
+	extern	printf
 
-	mov edx, len
-	mov ecx, msg
-	mov ebx, 1
-	mov eax, 4
 
-	mov eax, 1
-
-section .data
-	msg db "Hello, Holberton" ,10,0
-	len equ $ -msg
+main:
+	mov	rdi, msg
+	mov	rax, 0
+	call	printf
+	mov	rax, 0
+	ret
+msg:
+	db "Hello, Holberton", 10, 0
