a3b111d5dc21d11d2d8ff9bff83b8ceec146479e
Gatwan <abigailgathigia@gmail.com>
2022-10-24 17:42:28 +0000

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+*main - function that prints before the main executed
+*/
+int main(void)
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
