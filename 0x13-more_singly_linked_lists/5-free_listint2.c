40bae6ad045a0335e44976e376c7b75e159b8c1a
Gatwan <abigailgathigia@gmail.com>
2022-10-27 11:48:36 +0000

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+*free_listint2 - function that frees a list
+*
+*@head: first element on the list
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *list;
+
+	while (*head != NULL)
+	{
+		list = *head;
+		*head = (*head)->next;
+		free(list);
+	}
+	*head = NULL; 
+}
