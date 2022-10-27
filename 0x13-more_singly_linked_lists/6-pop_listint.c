ebd71d35976ad47a04ba3fd606d8c76ede100596
Gatwan <abigailgathigia@gmail.com>
2022-10-27 12:51:43 +0000

    delete first node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,11 +1,26 @@
 #include "lists.h"
 /**
-*int pop_listint
+*int pop_listint - function that deletes the head node of a list
 *
+*@head: first element on the list
 *
-*
+*Return: head node's data
 */
 int pop_listint(listint_t **head)
 {
+	listint_t *temp;
+	int new;
 
+	if (head == NULL)
+		return (0);
+
+	new = (*head)->n;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	} 
+	return (new);
 }
