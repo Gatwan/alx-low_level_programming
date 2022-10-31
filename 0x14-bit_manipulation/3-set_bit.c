7fd34bdbc05f14695ed158954920c766409c0f46
Gatwan <abigailgathigia@gmail.com>
2022-10-31 17:22:07 +0000

    fix error

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*set_bit - function that sets the value of a bit to 1 at a given index 
+*set_bit - function that sets the value of a bit to 1 at a given index
 *
 *@n: pointer to value
 *
