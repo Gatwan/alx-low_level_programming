d3ca84e51ce813d7558156f48d0c4f183dea4683
Gatwan <abigailgathigia@gmail.com>
2022-12-08 16:28:01 +0000

    List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*dlistint_len - List length
+*
+*@h: pointer to list
+*
+*Return: number of elements
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
