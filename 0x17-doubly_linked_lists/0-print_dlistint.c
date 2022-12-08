bf8beff59124ecf7e56c7742fdd3390bd2deab05
Gatwan <abigailgathigia@gmail.com>
2022-12-08 16:21:08 +0000

    Fix Betty style error

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-/*
+/**
 *print_dlistint - function that prints list
 *
 *@h: pointer to list
@@ -10,7 +10,7 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		count++;
